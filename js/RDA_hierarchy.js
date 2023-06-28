13d90ffce8f7e1bb1f985c36ae222de2d801c1c5
paynec4 <48925631+cspayne@users.noreply.github.com>
2023-06-27 17:45:40 -0700

    update rda styles

--- js/RDA_hierarchy.js
+++ js/RDA_hierarchy.js
@@ -17,3 +17,26 @@ for(i=0; i < link.length; i++) {
   });
 }
 
+var expand_link = document.getElementById("expand")
+/* toggle guidance caret when link is clicked (from prop list to prop detail) */
+expand_link.addEventListener("click", function(){
+  var tog = document.getElementsByClassName("caret")
+  for (i = 0; i < tog.length; i++) {
+    if (!tog[i].parentElement.querySelector(".nested").classList.contains("active")) {
+      tog[i].parentElement.querySelector(".nested").classList.toggle("active");
+      tog[i].classList.toggle("caret-down");
+    }
+  };
+})
+
+var collapse_link = document.getElementById("collapse")
+/* toggle guidance caret when link is clicked (from prop list to prop detail) */
+collapse_link.addEventListener("click", function(){
+  var tog = document.getElementsByClassName("caret")
+  for (i = 0; i < tog.length; i++) {
+    if (tog[i].parentElement.querySelector(".nested").classList.contains("active")) {
+      tog[i].parentElement.querySelector(".nested").classList.toggle("active");
+      tog[i].classList.toggle("caret-down");
+    }
+  };
+})
